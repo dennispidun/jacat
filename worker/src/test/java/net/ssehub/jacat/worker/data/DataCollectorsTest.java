d577aee54c6dc29b34e762a4f7fdfd7c632f516d
Dennis Pidun <dennis.pidun@uni-hildesheim.de>
2021-02-24 18:08:56 +0100

    add tests for data collectors

---
+++ worker/src/test/java/net/ssehub/jacat/worker/data/DataCollectorsTest.java
@@ -0,0 +1,15 @@
+package net.ssehub.jacat.worker.data;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DataCollectorsTest {
+
+    private DataCollectors dataCollectors;
+
+    @BeforeEach
+    void setUp() {
+        dataCollectors = new DataCollectors();
+    }
+}
